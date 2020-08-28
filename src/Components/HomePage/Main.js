d9fc8b5e1475d625b6aaa8a5120ae4aacdfd58ad
otsolap <59653348+otsolap@users.noreply.github.com>
2020-08-28 17:13:44 +0300

    for some reason my Main wasn't having Authentication. I fixed my issues and am getting ready to finish notifications component too.

--- src/Components/HomePage/Main.js
+++ src/Components/HomePage/Main.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 import AllPosts from '../Posts/AllPosts'
 import Notifications from './Notifications';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux'
+import { Redirect } from 'react-router-dom';
+
 class Main extends Component {
     render() {
-        const { posts } = this.props;
+        const { posts, auth } = this.props;
+        if (!auth.uid) return <Redirect to="/Login" />
 
         return (
             <div>
@@ -14,6 +20,19 @@ class Main extends Component {
     }
 }
 
+const mapStateToProps = (state) => {
+    return {
+        // tämä on meidän this, props ja staten muokkaaja. :-D
+        // se tarvitsee Firestorereducerin joka on Rootreducerissa.
+        auth: state.firebase.auth,
+    }
+}
 
-
-export default (Main)
+// connect on se työkalu, joka yhdistää propsit/statet reduxin storeen.
+// connect kysyy MIKÄ komponentti, joten täällä se on Allposts.
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        { collection: 'posts', orderBy: ['createdAt', 'desc'] }
+    ])
+)(Main)
