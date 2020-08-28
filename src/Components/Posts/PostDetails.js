20953ee00a7c107537f4f419d46d67b08621feb6
otsolap <59653348+otsolap@users.noreply.github.com>
2020-08-28 18:04:35 +0300

    image placeholder working and CSS slightly better. Problems were caused by having div classes closing in wrong places.

--- src/Components/Posts/PostDetails.js
+++ src/Components/Posts/PostDetails.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { placeholder } from '../../images/placeholder.jpg'
+import placeholder from '../../images/placeholder.jpg'
 import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux';
 import { Redirect } from 'react-router-dom';
 import moment from 'moment';
+import { Link } from 'react-router-dom'
 
 
 const PostSummary = (props) => {
@@ -18,14 +19,14 @@ const PostSummary = (props) => {
                         <div className="card-image">
                             <img src={placeholder} alt="placeholder" />
                         </div>
-                        <span className="card-title">{post.title}</span>
-                        <div className="card-content">
+                        <div className="card-content black-text">
+                            <span className="card-title">{post.title}</span>
                             <p>by {post.authorUserName}</p>
                             <p>{post.message}</p>
                             <span>{moment(post.createdAt.toDate()).calendar()}</span>
-                        </div>
-                        <div className="card-action">
-                            This should take the user back.
+                            <div className="card-action">
+                                <Link to={'/'} >Back to Mainpage</Link>
+                            </div>
                         </div>
                     </div>
                 </div >
--- src/Components/Posts/PostSummary.js
+++ src/Components/Posts/PostSummary.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { placeholder } from '../../images/placeholder.jpg'
+import placeholder from '../../images/placeholder.jpg'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
 
@@ -7,18 +7,18 @@ const PostSummary = ({ post }) => {
     return (
         <div className="row">
             <div className="col s12 m7">
-                <div className="card">
+                <div className="card medium">
                     <div className="card-image">
                         <img src={placeholder} alt="placeholder" />
-                        <span className="card-title">{post.title}</span>
                     </div>
-                    <div className="card-content">
-                        <span>Posted by {post.authorUserName}</span>
+                    <div className="card-content black-text">
+                        <span className="card-title">{post.title}</span>
+                        <p>Posted by {post.authorUserName}</p>
                         <p>{post.message}</p>
                         <span>{moment(post.createdAt.toDate()).calendar()}</span>
-                    </div>
-                    <div className="card-action">
-                        <Link to={'/post/' + post.id} key={post.id}>Read text</Link>
+                        <div className="card-action">
+                            <Link to={'/post/' + post.id} key={post.id}>Read text</Link>
+                        </div>
                     </div>
                 </div>
             </div>
